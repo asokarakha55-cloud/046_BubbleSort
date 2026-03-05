e5ba7f4d3400d895b388065bd4d625300d49eedb
asokarakha55-cloud <asokarakha55@gmail.com>
2026-03-05 15:26:01 +0700

    Start looping and screen output

--- Bubble_shortA.cpp
+++ Bubble_shortA.cpp
@@ -23,5 +23,11 @@ void input()
     cout << "====================" << endl;
     cout << "Masukkan Elemen Array" << endl;
     cout << "====================" << endl;
+
     for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> d;
+        arr[i] = d;
+    }
 }
