cdcf087468ea8f6be9928005bf0bf0b963bca66f
asokarakha55-cloud <asokarakha55@gmail.com>
2026-03-05 15:24:49 +0700

    Add screen output

--- Bubble_shortA.cpp
+++ Bubble_shortA.cpp
@@ -16,6 +16,12 @@ void input()
             break;
         else
         {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
         }
     }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "====================" << endl;
+    for (int i = 0; i < n; i++)
 }
