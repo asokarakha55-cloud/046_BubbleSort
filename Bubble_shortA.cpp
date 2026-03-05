5dea65e18ac8b730b0ed2445cb17ec26849012d5
asokarakha55-cloud <asokarakha55@gmail.com>
2026-03-05 15:18:12 +0700

    add input and output for element

--- Bubble_shortA.cpp
+++ Bubble_shortA.cpp
@@ -4,3 +4,18 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input()
+{
+    int d;
+    while (true)
+    {
+        cout << "masukkan banyak elemen padaa array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else
+        {
+        }
+    }
+}
